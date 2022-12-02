2f090040ab0c4f4eb038cd3fe54a21e6a0e2e8e4
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-12-02 23:12:45 +0600

    model and migration

---
+++ database/migrations/2022_12_02_170714_create_courses_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 125);
+            $table->string('slug', 125);
+            $table->longText('description');
+            $table->string('thumbnail', 125);
+            $table->longText('requirements');
+            $table->longText('audience');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->foreignId('category_id');
+            $table->foreignId('teacher_id');
+            $table->timestamps();
+        });
+
+        // courses users
+        Schema::create('courses_users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('course_id');
+            $table->timestamps();
+        });
+
+        // courses users wishlists
+        Schema::create('courses_users_wishlists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id');
+            $table->foreignId('course_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+        Schema::dropIfExists('courses_users');
+        Schema::dropIfExists('courses_users_wishlists');
+    }
+};
