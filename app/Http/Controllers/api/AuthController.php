b7d76199d2e966fb3644baef1e6e97baaa916f3d
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-12-12 21:29:16 +0600

    login and register api

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Validation\Rules\Password;
+
+Class AuthController extends Controller {
+
+    /**
+     * method register.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register( Request $request )
+    {
+        // validation
+        $request->validate([
+            'name'     => ['required', 'string', 'max:255'],
+            'email'    => ['required', 'string', 'email',  'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ]);
+
+        try {
+            // User Registration
+            $user = User::create ([
+                'name'     => $request->name,
+                'email'    => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+             // Response
+             return [
+                'error'   => false,
+                'message' => 'Registration Successfull!',
+                'user'    => new UserResource( $user ),
+                // 'verified' => 'A verification code has been send to your email'
+            ];
+
+            // Verify Code
+            // $code = random_int(100000, 999999);
+
+            // if ($user) {
+                // Store Verify Code
+                // UserVerification::create([
+                //     'user_id' => $user->id,
+                //     'code'    => $code,
+                // ]);
+
+                // Event Trigger
+                // event(new Registered($user, $code));
+
+                // Response
+                // return [
+                //     'error'    => false,
+                //     'message'  => 'Registration Successfull!',
+                //     'verified' => 'A verification code has been send to your email'
+                // ];
+            // }
+
+        } catch (\Throwable $th) {
+            // Response
+            return [
+                'error'    => true,
+                'message'  => $th->getMessage(),
+            ];
+        }
+    }
+
+    /**
+     * method login.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login( Request $request )
+    {
+        // validation
+        $request->validate([
+            'email'    => ['required', 'string', 'email',  'max:255'],
+            'password' => ['required'],
+        ]);
+
+        try {
+            // User Registration
+            if ( !Auth::attempt($request->only('email', 'password'))) {
+                // Response
+                return [
+                    'error'    => true,
+                    'message'  => 'Email/Password error!',
+                ];
+            }
+
+            $user = Auth::user();
+            /** @var User $user */
+
+            // Token
+            $token = $user->createToken('authToken')->plainTextToken;
+
+            // Response
+            return [
+                'error'   => false,
+                'message' => 'Login Successful',
+                'token'   => $token,
+                'user'    => new UserResource( $user ),
+            ];
+
+            // if ( $user->hasVerifiedEmail() ) {
+            //     $token = $user->createToken('authToken')->plainTextToken;
+            //     // Response
+            //     return [
+            //         'error'   => false,
+            //         'message' => 'Login Successful',
+            //         'token'   => $token,
+            //         'user'    => new UserResource( $user ),
+            //     ];
+            // } else {
+            //     // Response
+            //     return [
+            //         'error'   => false,
+            //         'message' => 'You need to verify your account',
+            //     ];
+            // }
+
+        } catch (\Throwable $th) {
+            // Response
+            return [
+                'error'    => true,
+                'message'  => $th->getMessage(),
+            ];
+        }
+    }
+
+    public function deleteUser( Request $request )
+    {
+        try {
+
+        } catch (\Throwable $th) {
+            //
+
+        }
+    }
+
+}
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+
+        return [
+            'user_id'     => $this->id,
+            'user_name'   => $this->name,
+            'user_email'  => $this->email,
+            'last_update' => $this->updated_at->format('d-M-y'),
+        ];
+    }
+}
